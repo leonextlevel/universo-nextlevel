8969bb6d67e63c878936f68a4af404fd8598444b
LeoNextLevel <leandrolopessjc15@gmail.com>
2021-09-03 23:53:42 -0300

    Detalhes do Personagem

--- universo_nextlevel/personagem/urls.py
+++ universo_nextlevel/personagem/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
 
-from .views import PersonagemListView, PersonagemCreateView, PersonagemUpdateView
+from .views import (
+    PersonagemListView,
+    PersonagemCreateView,
+    PersonagemUpdateView,
+    PersonagemDetailView,
+)
 
 urlpatterns = [
     path('list/', PersonagemListView.as_view(), name="personagem_list"),
     path('new/', PersonagemCreateView.as_view(), name="personagem_form"),
     path('update/<int:pk>/', PersonagemUpdateView.as_view(), name="personagem_form"),
+    path('detail/<int:pk>/', PersonagemDetailView.as_view(), name="personagem_detail"),
 ]
--- universo_nextlevel/personagem/views.py
+++ universo_nextlevel/personagem/views.py
@@ -1,9 +1,11 @@
+from typing import Any, Dict
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
     CreateView,
     UpdateView,
     TemplateView,
+    DetailView,
 )
 
 from .models import Personagem
@@ -23,8 +25,23 @@ class PersonagemCreateView(CreateView):
     form_class = PersonagemForm
     success_url = reverse_lazy('personagem_list')
 
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Criar Personagem'
+        return context
+
 
 class PersonagemUpdateView(UpdateView):
     model = Personagem
     form_class = PersonagemForm
     success_url = reverse_lazy('personagem_list')
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Atualizar Personagem'
+        return context
+
+
+class PersonagemDetailView(DetailView):
+    model = Personagem
+    context_object_name = 'personagem'
